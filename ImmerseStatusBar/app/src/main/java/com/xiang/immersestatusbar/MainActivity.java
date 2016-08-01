2e7489d5f47a8c77ba0f2fa31b09ab69e6352247
77377xiang <xiang792178903@sina.com>
2016-08-01 23:41:43 +0800

    状态沉浸栏   及 电池 型号栏

---
+++ ImmerseStatusBar/app/src/main/java/com/xiang/immersestatusbar/MainActivity.java
@@ -0,0 +1,19 @@
+package com.xiang.immersestatusbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //透明状态栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //透明导航栏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+    }
+}
